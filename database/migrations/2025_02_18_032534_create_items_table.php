f94f62eadf31d1ba607e085d900020d77a940e30
TERENZE DANE C. MAGRACIA <2200612@cc.usls.edu>
2025-02-20 11:26:10 +0800

    Feat: Seeding data into database,

--- database/migrations/2025_02_18_032534_create_items_table.php
+++ database/migrations/2025_02_18_032534_create_items_table.php
@@ -4,24 +4,27 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateItemsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('items', function (Blueprint $table) {
-            $table->id();
+            $table->id(); 
+            $table->string('name'); 
+            $table->decimal('price', 10, 2); 
+            $table->unsignedBigInteger('category_id'); 
             $table->timestamps();
+
+         
+            $table->foreign('category_id')
+                  ->references('id')
+                  ->on('categories')
+                  ->onDelete('CASCADE');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('items');
     }
-};
+}
